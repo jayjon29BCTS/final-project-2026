ef5d48139e22e184eac0fe4986f3aed28e56f7dc
Rishi Kapadia <riskap29@bergen.org>
2026-05-10 22:56:00 -0400

    finished upp css and js for the faction description menu. No issues, except for redundant css. Next, will add the rest for landing.html, and then begin styling and js for comments,html

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,14 @@
-const factionButton = document.querySelectorAll(toolbar)
+const factionButtons = document.querySelectorAll(".btn-container")
+const factionInfoSlides = document.querySelectorAll(".faction-info")
+const blueInfo = document.querySelector("#blue")
+const redInfo = document.querySelector("#red")
+const yellowInfo = document.querySelector("#yellow")
+const purpleInfo = document.querySelector("#purple")
+const whiteInfo = document.querySelector("#white")
+const greenInfo = document.querySelector("#green")
+let factionButtonsLength = 0
+factionButtons.forEach((btn)=>{
+    factionButtonsLength++
+})
+const colorKey = [redInfo, blueInfo, greenInfo, purpleInfo, yellowInfo, whiteInfo]
+
---
+++ js/script.js
@@ -0,0 +1,18 @@
+factionButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        factionButtons.forEach((btn) => btn.classList.remove("active"))
+        btn.classList.add("active")
+        displayText()
+    })
+})
+function displayText() {
+        factionButtons.forEach((btn, index) => {
+            if (btn.classList.contains("active")) {
+                for(let color of colorKey){
+                    color.style.display = "none"
+                }
+                colorKey[index].style.display = "block"
+            }
+        })
+
+}

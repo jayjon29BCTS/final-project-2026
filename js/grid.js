8f0ad63456472743c920ee88112aee9ae27305b9
Super Senior Gojo <jayjon29@bergen.org>
2026-05-14 15:06:38 -0400

    Update grid.js

--- js/grid.js
+++ js/grid.js
@@ -1,16 +1,29 @@
 gridForm.addEventListener("submit", (e)=>{
     e.preventDefault()
-    console.log(gridInput.value)
+    let rawText = gridInput.value
+    const text = rawText.split(" ")
+    const operandEl = document.querySelector(`#${text[0].toUpperCase()}`)
+    if(text[1] === "land") {
+        operandEl.style.backgroundColor = "green"
+    } else {
+        operandEl.style.backgroundColor = "blue"
+    }
+    if(colors.includes(text[2])) {
+        deleteOthers(operandEl)
+        const territory = document.createElement("div")
+        territory.style.width = "50%"
+        territory.style.height = "50%"
+        territory.style.backgroundColor = text[2]
+        territory.classList.add("territory-marking")
+        operandEl.appendChild(territory)
+    }
 })
-const str = "The quick brown fox jumps over the lazy dog.";
 
-const words = str.split(" ");
-console.log(words[3]);
-// Expected output: "fox"
+function deleteOthers(element){
+    const killList = element.querySelectorAll(".territory-marking")
+    console.log(killList)
+    killList.forEach(kid => {
+        kid.remove()
+    });
 
-const chars = str.split("");
-console.log(chars[8]);
-// Expected output: "k"
-
-const strCopy = str.split();
-console.log(strCopy);
+}

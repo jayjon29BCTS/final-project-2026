1657373eb509f43f66823bfafd612e2843a8f1a4
Rishi Kapadia <riskap29@bergen.org>
2026-05-22 00:45:28 -0400

    finished some of the rendering, and added a clear button functionality

--- js/dom.js
+++ js/dom.js
@@ -9,11 +9,78 @@ const greenInfo = document.querySelector("#green")
 const colorKey = [redInfo, blueInfo, greenInfo, purpleInfo, yellowInfo, whiteInfo]
 const gridForm = document.querySelector(".testing")
 const gridInput = document.querySelector("#grid-text")
-const colors = ["red", "blue", "green", "purple", "yellow", "white"]
+const colors = ["red", "blue", "green", "purple", "yellow", "white","blank"]
 let gridData = []
 for(let i=0; i<64; i++){
-    gridData.push({land:false, territory:"none", buildings:"none", army:false})
+    gridData.push({land:null, territory:"none", buildings:"none", army:false})
 }
 
-const buildings = ["hut","lumberyard", "garden", "embassy", "port", "refinery","quarry","barracks", "multicenter", "complex", "defenseArray"]
+const clearBtn = document.querySelector(".clear-btn")
 
+const buildings = ["hut","lumberyard", "garden", "embassy", "port", "refinery","quarry","barracks", "multicenter", "complex", "defenseArray"]
+function decompileInit(int){
+   const charOne = Math.floor(int/8)
+   const charTwo = int%8
+   let selector = []
+   switch(charOne) {
+    case 0:
+        selector.push("A")
+        break;
+    case 1:
+        selector.push("B")
+        break;
+    case 2:
+        selector.push("C")
+        break;
+    case 3:
+        selector.push("D")
+        break;
+    case 4:
+        selector.push("E")
+        break;
+    case 5:
+        selector.push("F")
+        break;
+    case 6:
+        selector.push("G")
+        break;
+    case 7:
+        selector.push("H")
+        break;
+   }
+   switch(charTwo) {
+    case 0:
+        selector.push("A")
+        break;
+    case 1:
+        selector.push("B")
+        break;
+    case 2:
+        selector.push("C")
+        break;
+    case 3:
+        selector.push("D")
+        break;
+    case 4:
+        selector.push("E")
+        break;
+    case 5:
+        selector.push("F")
+        break;
+    case 6:
+        selector.push("G")
+        break;
+    case 7:
+        selector.push("H")
+        break;
+   }
+   return selector
+}
+function deleteOthers(element){
+    const killList = element.querySelectorAll(".territory-marking")
+    killList.forEach(kid => {
+        kid.remove()
+    });
+    return
+
+}
--- js/grid.js
+++ js/grid.js
@@ -1,4 +1,4 @@
-
+loadSaved()
 gridForm.addEventListener("submit", (e)=>{
     e.preventDefault()
     let rawText = gridInput.value
@@ -7,95 +7,79 @@ gridForm.addEventListener("submit", (e)=>{
     const operandEl = document.querySelector(`#${text[0].toUpperCase()}`)
     let isTerritory = "none"
     let hasBuildings = "none"
-    let land = false
+    let land = null
 
     // check for land type
     if(text[1] === "land") {
         operandEl.style.backgroundColor = "rgb(113, 251, 138)"
-        !land
         land = true
     } else if(text[1]==="sea") {
         operandEl.style.backgroundColor = "blue"
+        land = false
     }
     // check for territory
     if((colors.includes(text[2])||colors.includes(text[1]))&& (text[1]!=="sea")) {
-        isTerritory=text[2||1]
+        isTerritory=text[2]||text[1]
         deleteOthers(operandEl)
-        territory = document.createElement("div")
-        territory.style.width = "80%"
-        territory.style.height = "80%"
-        territory.style.backgroundColor = text[2]||text[1]
-        territory.classList.add("territory-marking")
-        operandEl.appendChild(territory)
-            // check for buildings
-    if(buildings.includes(text[3])||buildings.includes(text[2]||buildings.includes(text[1]))) {
-            console.log("buildings")
-            territory.textContent = text[3]||text[2]||text[1]
-            hasBuildings= territory.textContent
-            territory.classList.add("building-text")
-    }
+    if (isTerritory!=="blank"){
+            const territory = document.createElement("div")
+            territory.style.width = "80%"
+            territory.style.height = "80%"
+            territory.style.backgroundColor = text[2]||text[1]
+            territory.classList.add("territory-marking")
+            operandEl.appendChild(territory)
+                // check for buildings
+        if(buildings.includes(text[3]) || buildings.includes(text[2]) || buildings.includes(text[1])) {
+                console.log("buildings")
+                territory.textContent = text[3]||text[2]||text[1]
+                hasBuildings= territory.textContent
+                territory.classList.add("building-text")
+        }
+    } 
 }
-    // if (colors.includes(text[1||2||3||4]+"army")){
-    //     console.log(text[1||2||3||4]+"army")
-    // } 
     saveToStorage(operandEl.id, land, isTerritory, hasBuildings)
 })
 
-function deleteOthers(element){
-    const killList = element.querySelectorAll(".territory-marking")
-    killList.forEach(kid => {
-        kid.remove()
-    });
-    return
 
-}
 function saveToStorage(elId, landType, whosTerritory, thereBuildings){
     const elIdA = elId.split("")[0]
     const elIdB = elId.split("")[1]
     console.log(elIdA, elIdB)
     idInt =  (elIdA.toLowerCase().charCodeAt(0) - 97)*8 + elIdB.toLowerCase().charCodeAt(0) - 97 ;
     console.log(idInt)
-    localStorage.setItem("storageData", JSON.stringify(gridData))
     gridData[idInt].land = landType
     gridData[idInt].territory = whosTerritory
     gridData[idInt].buildings= thereBuildings
-    console.log(gridData[idInt])    
+    localStorage.setItem("storageData", JSON.stringify(gridData))
 
 }
-function initialize() {
-    const rawGridInitial = localStorage.getItem("storageData")
-     for(let i=0;i<data;i++){
-         docompileInit(i)
-        
-        return
+function loadSaved() {
+    const rawGridInitial = JSON.parse(localStorage.getItem("storageData"))
+    gridData = rawGridInitial? rawGridInitial : gridData
+     for(let i=0;i<gridData.length;i++){
+         decompiledOperandEl = document.querySelector(`#${decompileInit(i)[0]+decompileInit(i)[1]}`)
+         if (gridData[i].land == true){
+            decompiledOperandEl.style.backgroundColor = "rgb(113, 251, 138)"
+         } else if(gridData[i].land == false){
+            decompiledOperandEl.style.backgroundColor = "blue"
+         }
+         const territory = document.createElement("div")
+
+            territory.style.width = "80%"
+            territory.style.height = "80%"
+            territory.style.backgroundColor = gridData[i].territory
+            territory.classList.add("territory-marking")
+            decompiledOperandEl.appendChild(territory)
+         
+
     }
 
 }
-function docompileInit(int){
-   const charOne = int/8
-   const charTwo = int%8
-   let selector = []
-   switch(charOne) {
-    case 0:
-        selector.push("A")
-        break;
-    case 1:
-        selector.push("B")
-        break;
-    case 2:
-        selector.push("C")
-        break;
-    case 3:
-        selector.push("D")
-        break;
-    case 4:
-        selector.push("A")
-        break;
-    case 0:
-        selector.push("A")
-        break;
-    case 0:
-        selector.push("A")
-        break;
-   }
+clearBtn.addEventListener("click", ()=>{
+    gridData = []
+    for(let i=0; i<64; i++){
+    gridData.push({land:null, territory:"none", buildings:"none", army:false})
 }
+localStorage.setItem("storageData", JSON.stringify(gridData))
+})
+
